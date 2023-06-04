fc8b825d6e296e53cb14b0706c3b374f9a185c85
Frank Huang <karokwbh2000@gmail.com>
2023-06-04 22:13:27 +0800

    candy010

---
+++ candy010.rb
@@ -0,0 +1,30 @@
+# 編號：CANDY-010
+# 程式語言：Ruby
+# 題目：把數字以 10 進位展開式呈現，數字均為大於 0 的正整數
+# 範例：9527 變成 "1000 x 9 + 100 x 5 + 10 x 2 + 7"
+
+def expandedForm(num)
+  num.to_s.chars.reverse.each_with_index.reduce("") { |sum, (x, i)|
+    if i == 0
+      if x != "0"
+        sum = x
+      end
+    else
+      if x != "0"
+        if sum == ""
+          sum = "#{10.pow(i)} x #{x}"
+        else
+          sum = "#{10.pow(i)} x #{x} + #{sum}"
+        end
+      end
+    end
+
+    sum
+  }
+end
+
+puts expandedForm(8) # 印出 8
+puts expandedForm(25) # 印出 10 x 2 + 5
+puts expandedForm(148) # 印出 100 x 1 + 10 x 4 + 8
+puts expandedForm(1450) # 印出 1000 x 1 + 100 x 4 + 10 x 5
+puts expandedForm(60308) # 印出 10000 x 6 + 100 x 3 + 8
