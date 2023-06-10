cdc39a17f9ad553fc04b74e1df171c12f047265a
Frank Huang <karokwbh2000@gmail.com>
2023-06-10 13:37:52 +0800

    candy018 optimize

--- candy018.rb
+++ candy018.rb
@@ -2,9 +2,10 @@
 # 程式語言：Ruby
 # 題目：實作一個可以印出隨機整數的函數
 
-def randomNumber(num1, num2 = 0)
-  num1, num2 = [num1, num2].min, [num1, num2].max
-  [*num1 .. num2].sample
+def randomNumber(num1, num2 = nil)
+  return rand(num1) if num2.nil?
+
+  rand(num1..num2)
 end
 
 p randomNumber(50) # 隨機印出 0 ~ 49 之間的任何一個數字
